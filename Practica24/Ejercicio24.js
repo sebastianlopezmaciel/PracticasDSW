68f05c84ddb83cf79e1f5ad95df71250fdd9a5ab
sebastianlopezmaciel <132530395+sebastianlopezmaciel@users.noreply.github.com>
2023-10-03 18:45:32 -0700

    Practica 25 en la cual utilizamos js y lo enlazamos a html para hacer una calculadora basica.

---
+++ Practica24/Ejercicio24.js
@@ -0,0 +1,35 @@
+function suma()
+{
+  var numero1 = 200;
+  var numero2 = 150;
+
+  var suma = numero1 + numero2;
+  return suma;
+}
+
+function resta()
+{
+    var numero1 = 200;
+    var numero2 = 150;
+
+  var resta = numero1 - numero2;
+  return resta;
+}
+
+function multiplicacion()
+{
+    var numero1 = 200;
+    var numero2 = 150;
+
+  var multiplicacion = numero1 * numero2;
+  return multiplicacion;
+}
+
+function division()
+{
+    var numero1 = 200;
+    var numero2 = 150;
+
+  var division = numero1 / numero2;
+  return division;
+}
