a206b6c5b0ef22e87eb3d953d6df89077bc024b4
Sebastian <132530395+sebastianlopezmaciel@users.noreply.github.com>
2023-11-25 02:47:50 -0800

    estaciones con respuesta en js

---
+++ Practica26/Practica26.js
@@ -0,0 +1,35 @@
+
+var num1 = prompt ("escribir una estacion en miniscula");
+var num2 = prompt ("escribir una estacion en miniscula");
+
+switch(numerosss) {
+
+
+case num1 <= num2:
+    alert("num1 no es mayor k num2");
+    break;
+
+
+    case num2 => 0:
+        alert("num2 es positivo");
+        break;
+
+        case ++num1 < num2 :
+    alert("incrementar uno en num1 , no lo hace mayor que 2");
+    break;
+
+    case "invierno":
+    alert("todo se hace sofocado");
+    break;
+
+ 
+    default:
+    alert("esto no es un numero")
+    break;
+
+
+
+
+
+
+}
